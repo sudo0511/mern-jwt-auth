539bc0638875d33ca4fe6e5f98e9e7c472b738ee
sudo0511 <shudhanshu0511max@gmail.com>
2025-11-29 02:07:49 +0530

    added logout route

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,6 +3,8 @@ import { createAccount, loginUser } from "../services/auth.service";
 import { CREATED, OK } from "../contants/http";
 import { setAuthCookies } from "../utils/cookies";
 import { loginSchema, registerSchema } from "./auth.schemas";
+import { verifyToken } from "../utils/jwt";
+import SessionModel from "../models/session.model";
 
 export const registerHandler = catchErrors(async (req, res) => {
   //validate request
@@ -28,5 +30,19 @@ export const loginHandler = catchErrors(async (req, res) => {
   const { accessToken, refreshToken } = await loginUser(request);
   return setAuthCookies({ res, accessToken, refreshToken }).status(OK).json({
     message: "Login Successful",
+  });
+});
+
+export const logoutHandler = catchErrors(async (req, res) => {
+  const accessToken = req.cookies.accessToken;
+  const { payload } = verifyToken(accessToken);
+  if (payload) {
+    await SessionModel.findByIdAndDelete(payload.sessionId);
+  }
+  res.clearCookie("accessToken").clearCookie("refreshToken", {
+    path: "/auth/refresh",
+  });
+  return res.status(OK).json({
+    message: "Logout successful",
   });
 });
--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,4 +1,4 @@
-import jwt, { SignOptions } from "jsonwebtoken";
+import jwt, { SignOptions, VerifyOptions } from "jsonwebtoken";
 import { JWT_REFRESH_SECRET, JWT_SECRET } from "../contants/env";
 import { SessionDocument } from "../models/session.model";
 import { UserDocument } from "../models/user.model";
@@ -29,4 +29,24 @@ export const signJwtToken = (
 ) => {
   const { secret, ...signOpt } = options || accessTokenSignOptions;
   return jwt.sign(payload, secret, { ...signOpt, ...defaults });
+};
+
+export const verifyToken = <TPayLoad extends object = AccessTokenPayload>(
+  token: string,
+  options?: VerifyOptions & { secret: string; audience?: string[] | string }
+) => {
+  const { secret = JWT_SECRET, ...verifyOpts } = options || {};
+  try {
+    const payload = jwt.verify(token, secret, {
+      audience: ["user"],
+      ...verifyOpts,
+    }) as TPayLoad;
+    return {
+      payload,
+    };
+  } catch (error: any) {
+    return {
+      error: error.message,
+    };
+  }
 };
